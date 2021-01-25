8e3c41654f119238436ff52baaf6dbfeb87168fe
BoyuanFeng <bfeng9@wisc.edu>
2021-01-24 20:45:12 -0800

    a working version for bmma. Tested for 6 matrix sizes with rand() initialized input matrices.

--- src/bmmaTensorCoreGemm/versions/10_bmmaTensorCoreGemm.cu
+++ src/bmmaTensorCoreGemm/versions/10_bmmaTensorCoreGemm.cu
@@ -94,9 +94,9 @@
 
 // GEMM configuration.
 
-#define M_TILES 512
-#define N_TILES 512
-#define K_TILES 64
+#define M_TILES 8
+#define N_TILES 16
+#define K_TILES 32
 
 #define M_GLOBAL (M * M_TILES)
 #define N_GLOBAL (N * N_TILES)
